1d81332b3f988845af92234bf48fcb6a1b9dcd9a
clifflyc <clifflyc@gmail.com>
2021-12-01 20:19:17 -0500

    Setup for running build locally :+1:

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import DashboardLayout from './layouts/DashboardLayout';
 import './vibe/scss/styles.scss';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route component={DashboardLayout} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
