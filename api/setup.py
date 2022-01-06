781feb5b778d6e87f99eb8eabe54a546fa68a85d
Ken Ming Lee <leekenming99@gmail.com>
2022-01-06 16:16:44 -0500

    change to beta version

--- api/setup.py
+++ api/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='realm_report',
-    version='0.0.1',
+    version='0.0.1-beta',
     description="REALM_AI Reporting Subsystem",
     install_requires=[
         "python-dotenv",
